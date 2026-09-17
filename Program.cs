cb579c2e1e0aeb58d2e44d91cc264e8dc6738416
danielseraph <agidigeorgedaniel9@gmail.com>
2026-09-17 15:51:03 +0100

    updated the core to 5173

--- Program.cs
+++ Program.cs
@@ -34,7 +34,7 @@ builder.Services.AddCors(options =>
         policy
             .SetIsOriginAllowed(origin =>
                 origin == "https://ai-resume-matcher-app.vercel.app" ||
-                origin == "http://localhost:4200")
+                origin == "http://localhost:5173")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
